abb5190da75d530e989d6aff3a3d2a4cb9c689b8
Mallory Ketcheson <mallory.ketcheson@gmail.com>
2014-10-27 11:42:42 -0400

    First
    Trail renderer with collider follows mouse when clicked.

---
+++ Assets/trailOnClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trailOnClick : MonoBehaviour {
+
+	public GameObject circle;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 10f));
+		circle.transform.position = new Vector3 (mousePos.x, mousePos.y, circle.transform.position.z);
+	
+		if (Input.GetMouseButton (0)) {
+			this.transform.position = new Vector3 (mousePos.x, mousePos.y, circle.transform.position.z);
+		}
+	}
+}
